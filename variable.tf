6d5321e6b98b64930d0545f4980290d1402bd75b
A0J6H <andrewhilton2006@gmail.com>
2024-06-12 12:55:41 +0100

    Reduced the ECS compute

--- variable.tf
+++ variable.tf
@@ -38,7 +38,7 @@ variable "app_port" {
 
 variable "app_count" {
     description = "Number of docker containers to run"
-    default = 3
+    default = 1
 }
 
 variable "health_check_path" {
@@ -47,10 +47,10 @@ variable "health_check_path" {
 
 variable "fargate_cpu" {
     description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
-    default = "1024"
+    default = "562"
 }
 
 variable "fargate_memory" {
     description = "Fargate instance memory to provision (in MiB)"
-    default = "2048"
+    default = "1024"
 }
